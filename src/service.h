933e7591e07fc3398f41bcc3e24c7e1a2ce8cf52
xiedd <xiedd@zjrealtech.com>
2018-06-29 09:32:07 +0800

    refactor(servmsg): move servmsg_init/close to servmsg.c

--- src/service.h
+++ src/service.h
@@ -26,43 +26,9 @@ struct servmsg {
 	int rc;
 };
 
-static inline void servmsg_init(struct servmsg *sm, struct spdnet_msg *msg,
-                                struct spdnet_node *snode)
-{
-	spdnet_msg_init(&sm->request);
-	spdnet_msg_move(&sm->request, msg);
-
-	void *sockid = zmq_msg_data(MSG_SOCKID(&sm->request));
-	size_t id_len = zmq_msg_size(MSG_SOCKID(&sm->request));
-	void *header = zmq_msg_data(MSG_HEADER(&sm->request));
-	size_t hdr_len = zmq_msg_size(MSG_HEADER(&sm->request));
-
-	// TODO: need performance optimization
-	spdnet_msg_init_data(&sm->response, sockid, id_len, NULL, 0, NULL, 0);
-	zmq_msg_close(MSG_HEADER(&sm->response));
-	zmq_msg_init_size(MSG_HEADER(&sm->response), hdr_len + 6);
-	memcpy(zmq_msg_data(MSG_HEADER(&sm->response)), header, hdr_len);
-	memcpy((char *)zmq_msg_data(MSG_HEADER(&sm->response)) + hdr_len,
-	       "_reply", 6);
-
-	sm->snode = snode;
-
-	if (!snode || snode->type == SPDNET_SUB) {
-		sm->reqserv = zmq_msg_data(MSG_SOCKID(&sm->request));
-		sm->reqserv_len = zmq_msg_size(MSG_SOCKID(&sm->request));
-	} else {
-		sm->reqserv = snode->id;
-		sm->reqserv_len = strlen(snode->id);
-	}
-
-	sm->rc = 0;
-}
-
-static inline void servmsg_close(struct servmsg *sm)
-{
-	spdnet_msg_close(&sm->request);
-	spdnet_msg_close(&sm->response);
-}
+void servmsg_init(struct servmsg *sm, struct spdnet_msg *msg,
+                  struct spdnet_node *snode);
+void servmsg_close(struct servmsg *sm);
 
 static inline const char *servmsg_reqid(struct servmsg *sm)
 {
