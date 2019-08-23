7baf068ec408860757a2045bcbb424afb84a98e9
yonzkon <yonzkon@gmail.com>
2019-08-23 17:40:54 +0800

    feat(napi): Add index.js

---
+++ napi/index.js
@@ -0,0 +1,33 @@
+const addon = require('./build/Release/bonfire').bonfire;
+const EventEmitter = require('events').EventEmitter;
+
+function Bonfire (address) {
+    this.addon = new addon(address);
+    this.addon.loop(1000);
+}
+
+Bonfire.prototype = new EventEmitter;
+
+Bonfire.prototype.loop = function(timeout) {
+    return this.addon.loop(timeout);
+};
+
+Bonfire.prototype.servcall = function(header, content) {
+    return this.addon.servcall(header, content);
+};
+
+Bonfire.prototype.publish = function(topic, content) {
+    return this.addon.publish(topic, content);
+};
+
+Bonfire.prototype.subscribe = function(topic) {
+    return this.addon.subscribe(topic, (content) => {
+        this.emit(topic, content);
+    });
+};
+
+Bonfire.prototype.unsubscribe = function(topic) {
+    return this.addon.unsubscribe(topic);
+};
+
+module.exports = Bonfire;
